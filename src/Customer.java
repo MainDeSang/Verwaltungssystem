518e948df1ff680d99105a25f00d1041d870d9c5
JeNesSaisPas <jenessaispasouplus@gmail.com>
2024-11-23 15:59:17 +0100

    Um Dokumentation erweitert.

--- src/Customer.java
+++ src/Customer.java
@@ -1,2 +1,29 @@
 public class Customer {
+
+    //Finale Klassenvariablen
+    private String name;
+    private int age;
+
+    
+    
+    //Konstruktor
+    public Customer(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+    
+    
+    //Setter und Getter Methoden
+    public String getName() {
+        return name;
+    }
+    public int getAge() {
+        return age;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,16 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        Book hdr = new Book("Herr der Ringer", "J.K Rolling", "turm", 19.00 , 1235234 );
+        Book book2 = new Book("Ein einsame Mann", "J.K Rolling", "turm", 19.00 , 1235234 );
+        Customer kunde =  new Customer("Anton", 24);
+
+
+        Library.setBook(hdr);
+        Library.setBook(book2);
+
+        Library.displayBooks();
+
     }
 }
